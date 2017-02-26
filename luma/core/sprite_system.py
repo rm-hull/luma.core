6f9e9b1d66d39ad8869708124fded14d473f2adb
Richard Hull <rm_hull@yahoo.co.uk>
2017-02-26 22:36:54 +0000

    Update docs

--- luma/core/sprite_system.py
+++ luma/core/sprite_system.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2014-17 Richard Hull and contributors
+# Copyright (c) 2017 Richard Hull and contributors
 # See LICENSE.rst for details.
+
+"""
+Simplified sprite animation framework.
+
+.. note:: this module is an evolving "work-in-progress" and should be treated
+          as such until such time as this notice disappears.
+"""
 
 import time
 from PIL import Image
@@ -22,42 +29,46 @@ class spritesheet(object):
     """
     A sprite sheet is a series of images (usually animation frames) combined
     into a larger image. A dictionary is usually spread into the object
-    constructor with the following attributes:
+    constructor parameters with the following top-level attributes:
 
-        * ``image``: a string representing the path to a sprite map image.
-        * ``frames``: a dictionary of settings that defines how to extract
-          individual frames from the supplied image, as follows
+    :param image: A path to a sprite map image.
+    :type image: str
 
-          - ``width`` & ``height`` are required and specify the dimensions of
-            the frames
-          - ``regX`` & ``regY`` indicate the registration point or "origin" of
-            the frames
-          - ``count`` allows you to specify the total number of frames in the
-            spritesheet; if omitted, this will be calculated based on the
-            dimensions of the source images and the frames. Frames will be
-            assigned indexes based on their position in the source images
-            (left to right, top to bottom).
+    :param frames: A dictionary of settings that defines how to extract
+        individual frames from the supplied image, as follows
 
-        * ``animations``: a dictionary of key/value pairs where the key is the
-          name of of the animation sequence, and the value are settings that
-          defines an animation sequence as follows:
+        - ``width`` & ``height`` are required and specify the dimensions of
+          the frames
+        - ``regX`` & ``regY`` indicate the registration point or "origin" of
+          the frames
+        - ``count`` allows you to specify the total number of frames in the
+          spritesheet; if omitted, this will be calculated based on the
+          dimensions of the source images and the frames. Frames will be
+          assigned indexes based on their position in the source images
+          (left to right, top to bottom).
+    :type frames: dict
 
-          - ``frames`` is a list of frame to show in sequence. Usually this
-            comprises of frame numbers, but can refer to other animation
-            sequences (which are handled much like a subroutine call).
-          - ``speed`` determines how quickly the animation frames are cycled
-            through compared to the how often the animation sequence yields.
-          - ``next`` is optional, but if supplied, determines what happens when
-            the animation sequence is exhausted. Typically this can be used to
-            self-reference, so that it forms an infinite loop, but can hand off
-            to any other animation sequence.
+    :param animations: A dictionary of key/value pairs where the key is the
+        name of of the animation sequence, and the value are settings that
+        defines an animation sequence as follows:
+
+        - ``frames`` is a list of frame to show in sequence. Usually this
+          comprises of frame numbers, but can refer to other animation
+          sequences (which are handled much like a subroutine call).
+        - ``speed`` determines how quickly the animation frames are cycled
+          through compared to the how often the animation sequence yields.
+        - ``next`` is optional, but if supplied, determines what happens when
+          the animation sequence is exhausted. Typically this can be used to
+          self-reference, so that it forms an infinite loop, but can hand off
+          to any other animation sequence.
+    :type animations: dict
 
     Loosely based on http://www.createjs.com/docs/easeljs/classes/SpriteSheet.html
     """
-    def __init__(self, **kwargs):
-        self.animations = dict_wrapper(kwargs['animations'])
-        self.frames = dict_wrapper(kwargs['frames'])
-        self.image = Image.open(kwargs['image'])
+    def __init__(self, image, frames, animations):
+        self.image = Image.open(image)
+        self.frames = dict_wrapper(frames)
+        self.animations = dict_wrapper(animations)
         # Reframe the sprite map in terms of the registration point (if set)
         regX = self.frames.regX if hasattr(self.frames, "regX") else 0
         regY = self.frames.regY if hasattr(self.frames, "regY") else 0
@@ -73,21 +84,27 @@ class spritesheet(object):
 
         self.cache = {}
 
-    def __getitem__(self, key):
+    def __getitem__(self, frame_index):
         """
-        Returns (and caches) the given frame. If a value outside the range is
-        given, an ``IndexError`` is raised.
+        Returns (and caches) the frame for the given index.
+        :param frame_index: The index of the frame.
+        :type frame_index: int
+        :returns: A Pillow image cropped from the main image corresponding to
+            the given frame index.
+        :raises TypeError: if the frame_index is not numeric
+        :raises IndexError: if the frame_index is less than zero or more
+            than the largest frame.
         """
 
-        if not isinstance(key, int):
+        if not isinstance(frame_index, int):
             raise TypeError("frame index must be numeric")
 
-        if key < 0 or key > self.frames.count:
+        if frame_index < 0 or frame_index > self.frames.count:
             raise IndexError("frame index out of range")
 
-        cached_frame = self.cache.get(key)
+        cached_frame = self.cache.get(frame_index)
         if cached_frame is None:
-            offset = key * self.frames.width
+            offset = frame_index * self.frames.width
             left = offset % self.width
             top = (offset // self.width) * self.frames.height
             right = left + self.frames.width
@@ -95,7 +112,7 @@ class spritesheet(object):
 
             bounds = [left, top, right, bottom]
             cached_frame = self.image.crop(bounds)
-            self.cache[key] = cached_frame
+            self.cache[frame_index] = cached_frame
 
         return cached_frame
 
@@ -105,15 +122,21 @@ class spritesheet(object):
         """
         return self.frames.count
 
-    def animate(self, key):
+    def animate(self, seq_name):
         """
         Returns a generator which "executes" an animation sequence for the given
-        key, inasmuch as the next frame for the given animation is yielded when
-        requested.
+        ``seq_name``, inasmuch as the next frame for the given animation is
+        yielded when requested.
+
+        :param seq_name: The name of a previously defined animation sequence.
+        :type seq_name: str
+        :returns: a generator that yields all frames from the animation
+            sequence.
+        :raises AttributeError: if the ``seq_name`` is unknown.
         """
         while True:
             index = 0
-            anim = getattr(self.animations, key)
+            anim = getattr(self.animations, seq_name)
             speed = anim.speed if hasattr(anim, "speed") else 1
             num_frames = len(anim.frames)
             while index < num_frames:
@@ -129,7 +152,7 @@ class spritesheet(object):
             if not hasattr(anim, "next"):
                 break
 
-            key = anim.next
+            seq_name = anim.next
 
 
 class framerate_regulator(object):
@@ -141,9 +164,11 @@ class framerate_regulator(object):
     ``sleep()`` method was called, and calculates a sleep period to smooth out
     the jitter.
 
-    By default the ``fps`` argument (frames-per-second) is set at 16.67, to give
-    a frame render time of 60ms. This can be overriden to user needs, and if no
-    FPS limiting is required, the ``fps`` can be set to zero.
+    :param fps: The desired frame rate, expressed numerically in
+        frames-per-second.  By default, this is set at 16.67, to give a frame
+        render time of approximately 60ms. This can be overriden as necessary,
+        and if no FPS limiting is required, the ``fps`` can be set to zero.
+    :type fps: float
     """
     def __init__(self, fps=16.67):
         if fps == 0:
