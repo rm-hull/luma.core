736dc536f27be6fbaa68204616243013de78017e
Richard Hull <rm_hull@yahoo.co.uk>
2017-03-06 22:24:01 +0000

    Add test to check canvas dither (#39)
    * Add test to check canvas dither
    * Update after PR comments

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -3,6 +3,10 @@
 # Copyright (c) 2017 Richard Hull and contributors
 # See LICENSE.rst for details.
 
+"""
+Tests for the :py:class:`luma.core.device.dummy` class.
+"""
+
 import os.path
 from PIL import Image, ImageChops
 
@@ -35,6 +39,25 @@ def test_portrait():
     # Use the same drawing primitives as the demo
     with canvas(device) as draw:
         baseline_data.primitives(device, draw)
+
+    bbox = ImageChops.difference(reference, device.image).getbbox()
+    assert bbox is None
+
+
+def test_dither():
+    reference = Image.open(
+        os.path.abspath(os.path.join(
+            os.path.dirname(__file__),
+            'reference',
+            'dither.png')))
+
+    device = dummy(mode="1")
+
+    with canvas(device, dither=True) as draw:
+        draw.rectangle((0, 0, 64, 32), fill="red")
+        draw.rectangle((64, 0, 128, 32), fill="yellow")
+        draw.rectangle((0, 32, 64, 64), fill="blue")
+        draw.rectangle((64, 32, 128, 64), fill="white")
 
     bbox = ImageChops.difference(reference, device.image).getbbox()
     assert bbox is None
