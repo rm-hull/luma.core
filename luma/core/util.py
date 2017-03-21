1bcb8932cb9776a5bc72cc0be307e20c624eedbb
Thijs Triemstra <info@collab.nl>
2017-03-21 23:51:31 +0100

    add monotonic for py27 osx (#60)

--- luma/core/util.py
+++ luma/core/util.py
@@ -3,6 +3,7 @@
 # See LICENSE.rst for details.
 
 import os
+import sys
 import time
 import warnings
 import ctypes.util
@@ -15,29 +16,50 @@ try:
     # only available since python 3.3
     monotonic = time.monotonic
 except AttributeError:
-    try:
-        clock_gettime = ctypes.CDLL(ctypes.util.find_library('c'),
-                                    use_errno=True).clock_gettime
-    except AttributeError:
-        clock_gettime = ctypes.CDLL(ctypes.util.find_library('rt'),
-                                    use_errno=True).clock_gettime
+    if sys.platform == 'darwin':  # OS X, iOS
+        libc = ctypes.CDLL('/usr/lib/libc.dylib', use_errno=True)
 
-    class timespec(ctypes.Structure):
-        """
-        Time specification, as described in clock_gettime(3).
-        """
-        _fields_ = (('tv_sec', ctypes.c_long),
-                    ('tv_nsec', ctypes.c_long))
+        class mach_timebase_info_data_t(ctypes.Structure):
+            """System timebase info. Defined in <mach/mach_time.h>."""
+            _fields_ = (('numer', ctypes.c_uint32),
+                        ('denom', ctypes.c_uint32))
 
-    def monotonic():
-        """
-        Monotonic clock that cannot go backward.
-        """
-        ts = timespec()
-        if clock_gettime(1, ctypes.pointer(ts)):
-            errno = ctypes.get_errno()
-            raise OSError(errno, os.strerror(errno))
-        return ts.tv_sec + ts.tv_nsec / 1.0e9
+        mach_absolute_time = libc.mach_absolute_time
+        mach_absolute_time.restype = ctypes.c_uint64
+
+        timebase = mach_timebase_info_data_t()
+        libc.mach_timebase_info(ctypes.byref(timebase))
+        ticks_per_second = timebase.numer / timebase.denom * 1.0e9
+
+        def monotonic():
+            """
+            Monotonic clock that cannot go backward.
+            """
+            return mach_absolute_time() / ticks_per_second
+    else:
+        try:
+            clock_gettime = ctypes.CDLL(ctypes.util.find_library('c'),
+                                        use_errno=True).clock_gettime
+        except AttributeError:
+            clock_gettime = ctypes.CDLL(ctypes.util.find_library('rt'),
+                                        use_errno=True).clock_gettime
+
+        class timespec(ctypes.Structure):
+            """
+            Time specification, as described in clock_gettime(3).
+            """
+            _fields_ = (('tv_sec', ctypes.c_long),
+                        ('tv_nsec', ctypes.c_long))
+
+        def monotonic():
+            """
+            Monotonic clock that cannot go backward.
+            """
+            ts = timespec()
+            if clock_gettime(1, ctypes.pointer(ts)):
+                errno = ctypes.get_errno()
+                raise OSError(errno, os.strerror(errno))
+            return ts.tv_sec + ts.tv_nsec / 1.0e9
 
 
 def deprecation(message):
