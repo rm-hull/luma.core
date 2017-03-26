9cef43ce47564dd4f2d4690685a805518dd1433d
Richard Hull <rm_hull@yahoo.co.uk>
2017-03-26 02:14:48 +0100

    Flake8 violations

--- luma/core/util.py
+++ luma/core/util.py
@@ -2,18 +2,8 @@
 # Copyright (c) 2017 Richard Hull and contributors
 # See LICENSE.rst for details.
 
-import time
 import warnings
-
-try:
-    # only available since python 3.3
-    monotonic = time.monotonic
-except AttributeError:
-    from monotonic import monotonic
-
 
 
 def deprecation(message):
     warnings.warn(message, DeprecationWarning, stacklevel=2)
-
-
