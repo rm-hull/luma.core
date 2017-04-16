8d011fdd1641d87a4fcccecb0efd08e65c8c132a
Thijs Triemstra <info@collab.nl>
2017-04-16 17:21:34 +0200

    document tests

--- tests/test_serial.py
+++ tests/test_serial.py
@@ -11,6 +11,9 @@ import pytest
 
 
 def test_module_deprecated():
+    """
+    The :py:mod:`luma.core.serial` module is deprecated.
+    """
     msg = ("luma.core.serial is deprecated and will be removed in " +
         "v1.0.0: use luma.core.interface.serial instead")
 
@@ -21,6 +24,10 @@ def test_module_deprecated():
 
 
 def test_module_all_equal():
+    """
+    :py:mod:`luma.core.serial`'s module provides the same ``__all__`` list
+    as :py:mod:`luma.core.interface.serial`.
+    """
     import luma.core.serial
     from luma.core.interface.serial import __all__
 
