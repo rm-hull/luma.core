ca75d76d05b2de25d3349887f2d4e07e1f8f19c4
Maarten <mlos@users.noreply.github.com>
2017-10-28 21:49:17 +0200

    multi-image support (#102)
    * Added multiimage.py
    * flake8 check
    * Do not render, just limit class to composition - image() returns an instance of the composed Image
    * Introduced RenderedImage
    * Renamed multi image to image composition
    * ImageComposition and ComposableImage
    * Added comments
    * Added assertions
    * Do not inherit from mixin.capabilities
    * Renamed dimensions to size
    * Check crop size for image()
    * Added more unit tests
    * Added refresh tests
    * Fixed documentation and formatting
    * cleanup after ImageDraw
    * use device.size

---
+++ luma/core/image_composition.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+"""
+Composes scrollable, positionable images into another
+Image
+
+.. versionadded:: 1.0.4
+"""
+
+from PIL import Image, ImageDraw
+
+
+class ComposableImage(object):
+    """
+    This class encapsulates an image and its attributes
+    that can be rendered onto an ``ImageComposition``
+    """
+    def __init__(self, image, position=(0, 0), offset=(0, 0)):
+        """
+        Instantiates a new ``ComposableImage``
+        :param image: the composable image
+        :type image: Image
+        :param position: the initial position of the image
+            within the composition
+        :type position: tuple
+        :param offset: the initial offset within the image,
+            from which it should be drawn
+        :type offset: tuple
+        """
+        assert(image)
+        self._image = image
+        self._position = position
+        self._offset = offset
+
+    @property
+    def position(self):
+        """
+        Getter for position
+        :returns: A tuple containing the x,y position
+        :rtype: tuple
+        """
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        """
+        Indicates where the image is to be rendered in an image
+        composition
+        :param value: The x,y position
+        :type value: tuple
+        """
+        self._position = value
+
+    @property
+    def offset(self):
+        """
+        Getter for offset
+        :returns: A tuple containing the top,left
+        :rtype: tuple
+        """
+        return self._offset
+
+    @offset.setter
+    def offset(self, value):
+        """
+        Indicates the top left position within the image,
+        as of which it is is to be rendered in the
+        image composition
+        :param value: The top,left position
+        :type value: tuple
+        """
+        self._offset = value
+
+    @property
+    def width(self):
+        """
+        :returns: The actual width of the image, regardless
+            its position or offset within the image composition
+        :rtype: int
+        """
+        return self._image.width
+
+    @property
+    def height(self):
+        """
+        :returns: The actual height of the image, regardless
+            its position or offset within the image composition
+        :rtype: int
+        """
+        return self._image.height
+
+    def image(self, size):
+        """
+        :param size: the width, height of the image composition
+        :type size: tuple
+        :returns: An image, cropped to the boundaries specified
+            by ``size``
+        :rtype: Image
+        """
+        assert(size[0])
+        assert(size[1])
+        return self._image.crop(box=self._crop_box(size))
+
+    def _crop_box(self, size):
+        """
+        Helper that calculates the crop box for the offset within the image
+        :param size: the width and height of the image composition
+        :type size: tuple
+        :returns: The bounding box of the image, given ```size```
+        :rtype: tuple
+        """
+        (left, top) = self.offset
+        right = left + min(size[0], self.width)
+        bottom = top + min(size[1], self.height)
+
+        return (left, top, right, bottom)
+
+
+class ImageComposition(object):
+    """
+    Manages a composition of ``ComposableImage``s that
+    can be rendered onto a single Image
+    """
+
+    def __init__(self, device):
+        """
+        Instantiates a new ``ImageComposition``
+
+        :param device: the device on which to render
+        :type device: Device
+        """
+        self._device = device
+        self._background_image = Image.new(device.mode, device.size)
+        self.composed_images = []
+
+    def add_image(self, image):
+        """
+        Adds an image to the composition
+        :param image: the image to add
+        :type image: Image
+        """
+        assert(image)
+        self.composed_images.append(image)
+
+    def remove_image(self, image):
+        """
+        Removes an image from the composition
+        :param image: the image to be removed
+        :type image: Image
+        """
+        assert(image)
+        self.composed_images.remove(image)
+
+    def __call__(self):
+        """
+        Returns the current composition
+        :rtype: Image
+        """
+        return self._background_image
+
+    def refresh(self):
+        """
+        Clears the composition and renders all the images
+        taking into account their position and offset
+        """
+        self._clear()
+        for img in self.composed_images:
+            self._background_image.paste(img.image(self._device.size),
+                                         img.position)
+        self._background_image.crop(box=self._device.bounding_box)
+
+    def _clear(self):
+        """
+        Helper that clears the composition
+        """
+        draw = ImageDraw.Draw(self._background_image)
+        draw.rectangle(self._device.bounding_box,
+                       fill="black")
+        del draw
---
+++ tests/test_image_composition.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+"""
+Tests for the :py:class:`luma.core.image_composition` class and associated
+helpers.
+"""
+
+import pytest
+import PIL
+
+from PIL import Image, ImageDraw
+from luma.core.device import dummy
+from luma.core.image_composition import ComposableImage, ImageComposition
+
+
+def test_composable_image_none():
+    with pytest.raises(AssertionError):
+        ComposableImage(None)
+
+
+def test_composable_image_image():
+    ci = ComposableImage(Image.new("RGB", (1, 1)))
+    with pytest.raises(AssertionError):
+        ci.image((0, 0))
+
+    with pytest.raises(AssertionError):
+        ci.image((1, 0))
+
+    with pytest.raises(AssertionError):
+        ci.image((0, 1))
+
+
+def test_composable_image_ctor():
+    pos = (78, 12)
+    offs = (90, 12)
+    img_size = (123, 234)
+    img = Image.new("RGB", img_size)
+    ci = ComposableImage(img)
+    ci.position = pos
+    ci.offset = offs
+    assert ci.position == pos
+    assert ci.offset == offs
+    assert ci.width == img_size[0]
+    assert ci.height == img_size[1]
+    assert isinstance(ci.image((1, 1)), PIL.Image.Image)
+
+
+def test_composable_image_crop_same():
+    img_size = (128, 64)
+    crop_size = img_size
+    img = Image.new("RGB", img_size)
+    ci = ComposableImage(img)
+    cropped_img = ci.image(crop_size)
+    assert cropped_img.size == crop_size
+
+
+def test_composable_image_crop_size_smaller_than_image_size():
+    img_size = (128, 64)
+    crop_size = (10, 10)
+    img = Image.new("RGB", img_size)
+    ci = ComposableImage(img)
+    cropped_img = ci.image(crop_size)
+    assert cropped_img.size == crop_size
+
+
+def test_composable_image_crop_size_greater_than_image_size():
+    img_size = (128, 64)
+    crop_size = (1000, 1000)
+    img = Image.new("RGB", img_size)
+    ci = ComposableImage(img)
+    cropped_img = ci.image(crop_size)
+    assert cropped_img.size == img_size
+
+
+def test_composable_image_crop_offset():
+    img_size = (128, 64)
+    crop_size = (10, 10)
+    img = Image.new("RGB", img_size)
+    ci = ComposableImage(img, offset=(20, 20))
+    cropped_img = ci.image(crop_size)
+    assert cropped_img.size == crop_size
+
+
+def test_image_composition_ctor():
+    ic = ImageComposition(dummy())
+    assert isinstance(ic(), PIL.Image.Image)
+
+
+def test_image_add_image_none():
+    ic = ImageComposition(dummy())
+    with pytest.raises(AssertionError):
+        ic.add_image(None)
+
+
+def test_image_remove_image_none():
+    ic = ImageComposition(dummy())
+    with pytest.raises(AssertionError):
+        ic.remove_image(None)
+
+
+def test_image_count():
+    ic = ImageComposition(dummy())
+    img1 = ComposableImage(Image.new("RGB", (1, 1)))
+    img2 = ComposableImage(Image.new("RGB", (1, 1)))
+    img3 = ComposableImage(Image.new("RGB", (1, 1)))
+    ic.add_image(img1)
+    ic.add_image(img2)
+    ic.add_image(img3)
+    assert len(ic.composed_images) == 3
+    ic.remove_image(img3)
+    assert len(ic.composed_images) == 2
+    ic.remove_image(img2)
+    assert len(ic.composed_images) == 1
+    ic.remove_image(img1)
+    assert len(ic.composed_images) == 0
+
+
+def test_refresh_no_images():
+    ic = ImageComposition(dummy())
+    ic_img_before = list(ic().getdata())
+    ic.refresh()
+    assert ic_img_before == list(ic().getdata())
+
+
+def test_refresh():
+    ic = ImageComposition(dummy())
+    ic_img_before = list(ic().getdata())
+    img = Image.new("RGB", (25, 25))
+    draw = ImageDraw.Draw(img)
+    draw.rectangle((10, 10, 20, 20), outline="white")
+    del draw
+    ci = ComposableImage(img)
+    ic.add_image(ci)
+    ic.refresh()
+    assert ic_img_before != list(ic().getdata())
