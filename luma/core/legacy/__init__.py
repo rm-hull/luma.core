8246b1c3568dbaa8a1887dea0b1cab7b3528a4e2
Thijs Triemstra <info@collab.nl>
2018-03-29 23:26:48 +0200

    doc update (#132)

--- luma/core/legacy/__init__.py
+++ luma/core/legacy/__init__.py
@@ -5,8 +5,8 @@
 """
 These methods were originally present in the old MAX7219 driver, and are
 preserved here only to aid migration away from that library. The functions are
-denoted 'legacy', to discourage use - you are encouraged to use the various
-drawing capabilities in Pillow instead.
+denoted 'legacy' to discourage use - you are encouraged to use the various
+drawing capabilities in the Pillow library instead.
 """
 
 from luma.core.render import canvas
@@ -21,9 +21,9 @@ def textsize(txt, font=None):
     This method is most useful for when the
     :py:class:`~luma.core.legacy.font.proportional` wrapper is used.
 
-    :param txt: the text string to calculate the bounds for
+    :param txt: The text string to calculate the bounds for
     :type txt: str
-    :param font: the font (from :py:mod:`luma.core.legacy.font`) to use
+    :param font: The font (from :py:mod:`luma.core.legacy.font`) to use
     """
     font = font or DEFAULT_FONT
     src = [c for ascii_code in txt for c in font[ord(ascii_code)]]
@@ -35,14 +35,14 @@ def text(draw, xy, txt, fill=None, font=None):
     Draw a legacy font starting at :py:attr:`x`, :py:attr:`y` using the
     prescribed fill and font.
 
-    :param draw: a valid canvas to draw the text onto.
+    :param draw: A valid canvas to draw the text onto.
     :type draw: PIL.ImageDraw
-    :param txt: the text string to display (must be ASCII only)
+    :param txt: The text string to display (must be ASCII only)
     :type txt: str
-    :param xy: an (x, y) tuple denoting the top-left corner to draw the text
+    :param xy: An ``(x, y)`` tuple denoting the top-left corner to draw the text
     :type xy: tuple
-    :param fill: the fill color to use (standard Pillow color name or RGB tuple)
-    :param font: the font (from :py:mod:`luma.core.legacy.font`) to use
+    :param fill: The fill color to use (standard Pillow color name or RGB tuple)
+    :param font: The font (from :py:mod:`luma.core.legacy.font`) to use
     """
     font = font or DEFAULT_FONT
     x, y = xy
@@ -60,14 +60,14 @@ def show_message(device, msg, y_offset=0, fill=None, font=None, scroll_delay=0.0
     """
     Scrolls a message right-to-left across the devices display.
 
-    :param device: the device to scroll across
-    :param msg: the text message to display (must be ASCII only)
+    :param device: The device to scroll across
+    :param msg: The text message to display (must be ASCII only)
     :type msg: str
-    :param y_offset: the row to use to display the text
+    :param y_offset: The row to use to display the text
     :type y_offset: int
-    :param fill: the fill color to use (standard Pillow color name or RGB tuple)
-    :param font: the font (from :py:mod:`luma.core.legacy.font`) to use
-    :param scroll_delay: the number of seconds to delay between scrolling
+    :param fill: The fill color to use (standard Pillow color name or RGB tuple)
+    :param font: The font (from :py:mod:`luma.core.legacy.font`) to use
+    :param scroll_delay: The number of seconds to delay between scrolling
     :type scroll_delay: float
     """
     fps = 0 if scroll_delay == 0 else 1.0 / scroll_delay
