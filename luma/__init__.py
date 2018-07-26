2eecd764c294f2adebb4728d92ac869662977ae7
Philip Howard <phil@gadgetoid.com>
2018-07-26 11:17:00 +0100

    Fix for setup.py declare_namespace error

--- luma/__init__.py
+++ luma/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017-18 Richard Hull and contributors
+# Copyright (c) 2014-18 Richard Hull and contributors
 # See LICENSE.rst for details.
 
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
+try:
+    from pkg_resources import declare_namespace
+    declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
