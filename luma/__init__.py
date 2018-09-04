21b64ee1b6ed21b888246b9383036c3fa767e955
Philip Howard <phil@gadgetoid.com>
2018-09-04 21:31:54 +0100

    Namespace packaging fix (#146)
    * Add namespace_packages back
    
    * Fix for setup.py declare_namespace error
    
    * Corrected date
    
    * Update __init__.py

--- luma/__init__.py
+++ luma/__init__.py
@@ -2,5 +2,15 @@
 # Copyright (c) 2017-18 Richard Hull and contributors
 # See LICENSE.rst for details.
 
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
+# declare_namespace(__name__) is required for setup.py to build a package
+# that correctly specifies "luma" in both "packages" and "namespace_packages"
+# Normally just adding "luma" as a regular package would suffice, but since
+# the namespace packages use a period "." they share a parent directory under
+# some circumstances and this file will be deleted when any one package is
+# uninstalled- breaking any remaining packages that share the "luma" directory.
+try:
+    from pkg_resources import declare_namespace
+    declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
