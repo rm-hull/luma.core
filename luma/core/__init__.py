4699c7fc4b5fd9e0021acc7b794909d9045fe41d
Thijs Triemstra <info@collab.nl>
2022-10-17 09:59:34 +0200

    release: 2.4.0 (#258)
    * bump version: 2.4.0
    
    * update changelog
    
    * only include luma dir in wheel

--- luma/core/__init__.py
+++ luma/core/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017-2020 Richard Hull and contributors
+# Copyright (c) 2017-2022 Richard Hull and contributors
 # See LICENSE.rst for details.
 
-__version__ = '2.3.2'
+__version__ = '2.4.0'
