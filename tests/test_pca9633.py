1506fa922167332b03e12f5e646df58db1387a2b
CutterXYZ <CutterXYZ@users.noreply.github.com>
2025-12-12 00:37:42 +0100

    Added support for AiP31068 and PCA9633 (#292)
    * Added support for AiP31068 and PCA9633
    
    * Fixed amber color channel if set to 0
    
    * Fixed spaces and indentation
    
    * Added unit tests
    
    * A few more tests
    
    * Fix pca9633 tests
    
    * Flake8 formatting
    
    * Removed comments

---
+++ tests/test_pca9633.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017-25 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+"""
+Tests for the :py:class:`luma.core.interface.serial.pca9633` class.
+"""
+
+from unittest.mock import Mock, call, ANY
+from luma.core.interface.serial import pca9633
+
+REG_MODE1 = 0x00
+REG_MODE2 = 0x01
+REG_LEDOUT = 0x08
+
+REG_RED_PWM = 0x04    # PWM2
+REG_GREEN_PWM = 0x03  # PWM1
+REG_BLUE_PWM = 0x02   # PWM0
+REG_GRP_PWM = 0x06    # GRPPWM
+
+smbus = Mock(unsafe=True)
+
+
+def setup_function():
+    smbus.reset_mock()
+
+
+def test_init():
+    pca9633(bus=smbus)
+
+    calls = [call(i2c_addr=ANY, register=REG_MODE1, value=ANY)] + \
+        [call(i2c_addr=ANY, register=REG_MODE2, value=ANY)] + \
+        [call(i2c_addr=ANY, register=REG_LEDOUT, value=ANY)]  # Do not enforce any settings
+
+    smbus.write_byte_data.assert_has_calls(calls)
+
+
+def test_enable_backlight():
+    backlight = pca9633(bus=smbus)
+    backlight(True)
+
+    calls = [call(i2c_addr=ANY, register=REG_RED_PWM, value=255)] + \
+        [call(i2c_addr=ANY, register=REG_GREEN_PWM, value=255)] + \
+        [call(i2c_addr=ANY, register=REG_BLUE_PWM, value=255)]
+
+    smbus.write_byte_data.assert_has_calls(calls)
+
+
+def test_set_brightness():
+    backlight = pca9633(bus=smbus)
+    backlight(True)
+    backlight.set_brightness(128)
+
+    calls = [call(i2c_addr=ANY, register=REG_GRP_PWM, value=128)]
+
+    smbus.write_byte_data.assert_has_calls(calls)
+
+
+def test_set_color():
+    backlight = pca9633(bus=smbus)
+    backlight(True)
+    backlight.set_color(0, 64, 128)
+
+    calls = [call(i2c_addr=ANY, register=REG_RED_PWM, value=0)] + \
+        [call(i2c_addr=ANY, register=REG_GREEN_PWM, value=64)] + \
+        [call(i2c_addr=ANY, register=REG_BLUE_PWM, value=128)]
+
+    smbus.write_byte_data.assert_has_calls(calls)
+
+
+def test_set_brightness_with_backlight_disabled():
+    backlight = pca9633(bus=smbus)
+    backlight(False)
+    backlight.set_brightness(128)
+
+    calls = [call(i2c_addr=ANY, register=REG_GRP_PWM, value=128)]
+
+    assert calls not in smbus.write_byte_data.mock_calls
+
+
+def test_set_color_with_backlight_disabled():
+    backlight = pca9633(bus=smbus)
+    backlight(False)
+    backlight.set_color(1, 64, 128)
+
+    calls = [call(i2c_addr=ANY, register=REG_RED_PWM, value=1)] + \
+        [call(i2c_addr=ANY, register=REG_GREEN_PWM, value=64)] + \
+        [call(i2c_addr=ANY, register=REG_BLUE_PWM, value=128)]
+
+    for c in calls:
+        assert c not in smbus.write_byte_data.mock_calls
+
+
+def test_brightness_restored_when_enabled():
+    backlight = pca9633(bus=smbus)
+    backlight(False)
+    backlight.set_brightness(128)
+    backlight(True)
+
+    calls = [call(i2c_addr=ANY, register=REG_GRP_PWM, value=128)]
+
+    smbus.write_byte_data.assert_has_calls(calls)
+
+
+def test_color_restored_when_enabled():
+    backlight = pca9633(bus=smbus)
+    backlight(False)
+    backlight.set_color(1, 64, 128)
+    backlight(True)
+
+    calls = [call(i2c_addr=ANY, register=REG_RED_PWM, value=1)] + \
+        [call(i2c_addr=ANY, register=REG_GREEN_PWM, value=64)] + \
+        [call(i2c_addr=ANY, register=REG_BLUE_PWM, value=128)]
+
+    smbus.write_byte_data.assert_has_calls(calls)
+
+
+def test_cleanup():
+    backlight = pca9633(bus=smbus)
+    backlight._managed = True
+    backlight.cleanup()
+    smbus.close.assert_called_once()
